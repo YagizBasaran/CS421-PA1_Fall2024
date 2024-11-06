b9aac6008995b4df506fa6f9f9fde5cdda61502d
Yagiz Basaran <91428580+YagizBasaran@users.noreply.github.com>
2024-11-06 22:34:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "2210378adsadsad2"
+    return "22103782"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
